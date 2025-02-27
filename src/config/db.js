7331b31f4dc4782a4a0336296ac09237bc72bf36
shubhamsavner-1011 <shubhamsavner@thoughtwin.com>
2025-02-27 01:39:39 -0600

    fix: db connenction

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 const url = process.env.DB_CONNECT
+console.log('url', url);
 mongoose.set({strictQuery:true})
 mongoose.connect(`${url}`, {useNewUrlParser:true}).then(()=>{
     console.log("connected")
-}).catch(()=>{
+}).catch((err)=>{
+    console.log(err);
     console.log("not connected")
 })
